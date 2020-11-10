1658d276ff9be00ad007cf28e125d5bf200454ab
lheritier.adam <lheritier.adam@gmail.com>
2020-11-10 08:35:51 +0100

    Finalisation classes Boisson et Commande

--- app/src/main/java/Models/Boisson.java
+++ app/src/main/java/Models/Boisson.java
@@ -1,10 +1,12 @@
 package Models;
 
-public class Boisson {
-    int _id;
-    String _nom;
-    int _volumeCl;
-    double _prix;
+import java.io.Serializable;
+
+public class Boisson implements Serializable {
+    private String _nom;
+    private int _volumeCl;
+    private double _prix;
+    private int _imageResourceId;
 
     public int get_id() {
         return _id;
@@ -26,12 +28,23 @@ public class Boisson {
         this._prix = _prix;
     }
 
-    public Boisson(int unId, String unNom, int unVolumeCl, double unPrix)
+    public Boisson(int unId, String unNom, int unVolumeCl, double unPrix, int uneImageResourceId)
     {
         _id = unId;
         _nom = unNom;
         _volumeCl = unVolumeCl;
         _prix = unPrix;
+        _imageResourceId= uneImageResourceId;
     }
 
+    @Override
+    public String toString()
+    {
+        return "Boisson{" +
+                "_id=" + _id +
+                ", _nom='" + _nom + '\'' +
+                ", _volumeCl=" + _volumeCl +
+                ", _prix=" + _prix +
+                '}';
+    }
 }
--- app/src/main/java/Models/Commande.java
+++ app/src/main/java/Models/Commande.java
@@ -1,11 +1,15 @@
 package Models;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Commande {
+public class Commande implements Serializable {
+    private int _numeroCommande;
     private ArrayList<Boisson> _lesBoissons;
 
-    public Commande() {
+    public Commande(int unNumeroCommande)
+    {
+        _numeroCommande = unNumeroCommande;
         _lesBoissons = new ArrayList<>();
     }
 
@@ -14,13 +18,28 @@ public class Commande {
         _lesBoissons.add(uneBoisson);
     }
 
+    public void clearOrder()
+    {
+        _lesBoissons.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Commande{" +
+                "_numeroCommande=" + _numeroCommande +
+                ", _lesBoissons=" + _lesBoissons + '}';
+    }
+
     public double prixCommande()
     {
         double prix = 0;
         for(Boisson boisson : _lesBoissons)
         {
-            prix += boisson._prix;
+            prix += boisson.get_prix();
         }
         return prix;
+
+
+
     }
 }
