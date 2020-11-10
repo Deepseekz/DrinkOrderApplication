424fc4417c0758974ca3cbdb0f314c444e8f394d
lheritier.adam <lheritier.adam@gmail.com>
2020-11-10 09:48:51 +0100

    Array adapter created

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -17,44 +18,36 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private Button btnAdd, btnDelete, btnValid, btnCancel;
-    private Spinner spDrink;
-    private TextView tvOrder;
-    private ListView lvOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-        btnValid = findViewById(R.id.btnValid);
-        btnDelete = findViewById(R.id.btnDelete);
-        btnCancel = findViewById(R.id.btnCancel);
-        btnAdd = findViewById(R.id.btnAdd);
-
-        initialisationSpinner();
-        initialisationListView();
+        initSpinner();
+        initListView();
     }
 
-    private void initialisationListView()
+    private void initListView()
     {
 
     }
 
-    public void initialisationSpinner()
+    public void initSpinner()
     {
-        spDrink = findViewById(R.id.spDrinks);
         ArrayList<Drink> drinks = new ArrayList<>();
 
-        drinks.add(new Drink( "Café", 5, 1, 0));
-        drinks.add(new Drink( "Thé Vert", 5, 1, 1));
-        drinks.add(new Drink( "Thé Noir", 5, 1, 2));
-        drinks.add(new Drink( "Cappuccino", 5, 1, 3));
-        drinks.add(new Drink( "Chocolat Chaud", 5, 1, 4));
-        drinks.add(new Drink( "Tisane", 5, 1, 5));
+        drinks.add(new Drink( "Coffee", 5, 1.4, 0));
+        drinks.add(new Drink( "Green Tea", 5, 3.5, 1));
+        drinks.add(new Drink( "Black Tea", 5, 2.6, 2));
+        drinks.add(new Drink( "Cappuccino", 5, 3.2, 3));
+        drinks.add(new Drink( "Hot Chocolate", 5, 3.4, 4));
+        drinks.add(new Drink( "Ice Tea", 5, 2, 5));
+
+        ArrayAdapter<Drink> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, drinks);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        binding.spDrinks.setAdapter(adapter);
     }
 }
